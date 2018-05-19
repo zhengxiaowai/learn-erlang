b46be2dacded4fc092048859e85aa7fe3ed3993f
zhengxiaowai <h1x2y3awalm@gmail.com>
2018-05-19 23:38:36 +0800

    #

---
+++ my_fac_server.erl
@@ -0,0 +1,15 @@
+-module(my_fac_server).
+-export([loop/0]).
+
+loop() ->
+    receive
+        {From, {fac, N}} ->
+            From ! {self(), fac(N)},
+            loop();
+        {become, Somthing} ->
+            Somthing()
+    end.
+
+
+fac(0) -> 1;
+fac(N) -> N * fac(N-1).
